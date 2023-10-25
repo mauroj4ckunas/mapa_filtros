70fbb2ea27344d3a7e77ef27951a279d1524c4d0
mjackunas <jackunasmauro@gmail.com>
2023-10-24 21:25:39 -0300

    primer filtro: por provincia

--- map.js
+++ map.js
@@ -25,33 +25,149 @@ document.querySelectorAll('.leaflet-sidebar-close').forEach(closeButton => {
     });
 });
 
-function onMapClick(e) {
-    console.log( e.latlng);
+const provincias = [
+    { nombre: 'Buenos Aires', clave: 'buenos_aires' },
+    { nombre: 'CABA', clave: 'caba' },
+    { nombre: 'Catamarca', clave: 'catamarca' },
+    { nombre: 'Chaco', clave: 'chaco' },
+    { nombre: 'Chubut', clave: 'chubut' },
+    { nombre: 'Córdoba', clave: 'cordoba' },
+    { nombre: 'Corrientes', clave: 'corrientes' },
+    { nombre: 'Entre Ríos', clave: 'entre_rios' },
+    { nombre: 'Formosa', clave: 'formosa' },
+    { nombre: 'Jujuy', clave: 'jujuy' },
+    { nombre: 'La Pampa', clave: 'la_pampa' },
+    { nombre: 'La Rioja', clave: 'la_rioja' },
+    { nombre: 'Mendoza', clave: 'mendoza' },
+    { nombre: 'Misiones', clave: 'misiones' },
+    { nombre: 'Neuquén', clave: 'neuquen' },
+    { nombre: 'Río Negro', clave: 'rio_negro' },
+    { nombre: 'Salta', clave: 'salta' },
+    { nombre: 'San Juan', clave: 'san_juan' },
+    { nombre: 'San Luis', clave: 'san_luis' },
+    { nombre: 'Santa Cruz', clave: 'santa_cruz' },
+    { nombre: 'Santa Fe', clave: 'santa_fe' },
+    { nombre: 'Santiago del Estero', clave: 'santiago_del_estero' },
+    { nombre: 'Tierra del Fuego', clave: 'tierra_del_fuego' },
+    { nombre: 'Tucumán', clave: 'tucuman' }
+];
+
+const dataProv = [
+    {
+        coordinate: [-34.74317133345502, -58.38993950186129],
+        provincia: 'buenos_aires',
+        title: 'Proyecto Mas',
+        description: 'Descripcion del Proyecto Max'
+    },
+    {
+        coordinate: [-29.14662871017508, -59.63440941489629],
+        provincia: 'santa_fe',
+        title: 'LOS CORTADEÑOS',
+        description: 'Descripcion del LOS CORTADEÑOS'
+    },
+    {
+        coordinate: [-27.473908430391607, -59.01214432071524],
+        provincia: 'chaco',
+        title: 'CORAZÓN AMIGO',
+        description: 'Descripcion del CORAZÓN AMIGO'
+    },
+    {
+        coordinate: [-23.812945916463313, -64.79712449468718],
+        provincia: 'jujuy',
+        title: 'CENTRO LIBERTAD',
+        description: 'Descripcion del CENTRO LIBERTAD'
+    },
+    {
+        coordinate: [-24.707747650774493, -60.58868413449087],
+        provincia: 'Formosa',
+        title: 'ASOCIACIÓN PARA LA PROMOCIÓN DE LA CULTURA Y EL DESARROLLO',
+        description: 'Descripcion del ASOCIACIÓN PARA LA PROMOCIÓN DE LA CULTURA Y EL DESARROLLO'
+    },
+    {
+        coordinate: [-31.54069031461898, -68.52747512168037],
+        provincia: 'san_juan',
+        title: 'Refugio Madre Teresa de Calculta',
+        description: 'Descripcion del Refugio Madre Teresa de Calculta'
+    },
+    {
+        coordinate: [-38.96534906483482, -68.06152514521344],
+        provincia: 'neuquen',
+        title: 'Refugio Cura Brochero',
+        description: 'Descripcion del Refugio Cura Brochero'
+    },
+];
+
+
+const markerGroup = L.layerGroup();
+function addMarker(key, value) {
+    markerGroup.clearLayers();
+    dataProv.forEach(prov => {
+        if (key === 'all' || key === prov[value]) {
+            const marker = L.marker(prov.coordinate).bindPopup(`
+                <div class="container">
+                    <p class="font-weight-bold mb-2">${prov.title}</p>
+                    <p class="font-italic">${prov.description}</p>
+                </div>
+            `);
+            markerGroup.addLayer(marker)
+        }
+    })
+    markerGroup.addTo(map);    
 }
 
-map.on('click', onMapClick);
+addMarker('all');
+function filtros(sidebar, provincias) {
 
-const userGroup = L.layerGroup();
+    let opcionesProv = '';
 
-function addMarkerUser() {
-    const mark1 = L.marker([-34.7407, -59.6422]);
-    userGroup.addLayer(mark1);
-    const mark2 = L.marker([-33.6502, -61.9383]);
-    userGroup.addLayer(mark2);
-    const mark3 = L.marker([-32.5826, -65.6950]);
-    userGroup.addLayer(mark3);
-    const mark4 = L.marker([-37.3851, -64.7718]);
-    userGroup.addLayer(mark4);
+    provincias.forEach(p => {
+        opcionesProv += `
+            <option value="${p.clave}">${p.nombre}</option>
+        `
+    });
+
+    const inputs = `
+        <div class="container h-100 w-100 my-4">
+            <div class="mb-3">
+                <label for="selectProvincias" class="form-label">Por provincia</label>
+                <select class="custom-select" id="selectProvincias">
+                    <option selected>Elije una provincia</option>
+                    ${opcionesProv}
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="selectProvincias2" class="form-label">Provincias 2</label>
+                <select class="custom-select" id="selectProvincias2">
+                    <option selected>Elije una provincia</option>
+                    ${opcionesProv}
+                </select>
+            </div>
+            <div class="row mt-10">
+                <div class="col-md-12">
+                    <button type="button" class="btn btn-info float-right" id="reiniciarFiltro">
+                        Reiniciar filtros
+                    </button>
+                </div>
+            </div>
+        </div>
+    `;
+
+    let panelContent = {
+        id: 'userinfo',                     // UID, used to access the panel
+        tab: '<i class="fa fa-search"></i>',  // content can be passed as HTML string,
+        pane: inputs,      // DOM elements can be passed, too
+        title: 'Aplicar filtros',              // an optional pane header
+        position: 'top'                  // optional vertical alignment, defaults to 'top'
+    };
+
+    sidebar.addPanel(panelContent);
+
+    document.querySelector("#selectProvincias").addEventListener("change", function (e) {
+        const provinciaSeleccionada = e.target.value;
+        addMarker(provinciaSeleccionada, 'provincia');
+    });
+    document.querySelector("#reiniciarFiltro").addEventListener("click", e => location.reload());
+
 }
 
-addMarkerUser();
-userGroup.addTo(map);
-
-let panelContent = {
-    id: 'userinfo',                     // UID, used to access the panel
-    tab: '<i class="fa fa-search"></i>',  // content can be passed as HTML string,
-    pane: '<p>Que ondqa perro</p>',      // DOM elements can be passed, too
-    title: 'Your Profile',              // an optional pane header
-    position: 'top'                  // optional vertical alignment, defaults to 'top'
-};
-sidebar.addPanel(panelContent);
+filtros(sidebar, provincias);
