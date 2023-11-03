0159234900d545a3ed5d6707e339b645297fd430
mjackunas <mjackunas@mail.ejercito.mil.ar>
2023-11-03 17:08:23 -0300

    fix: concatencacion de filtros + diseño

--- src/map.js
+++ src/map.js
@@ -1,8 +1,13 @@
 import { provincias } from "./utils/filtroProv.js";
 import { localidades } from "./utils/filtroLocalidad.js";
-import { tipoOrganizacion } from "./utils/filtroTipo.js";
 import { asistencias } from "./utils/filtroAsistencia.js";
 import { data } from "./utils/mock.js";
+
+const allFiltros = {
+    provincia: provincias,
+    localidad: localidades,
+    tipo: asistencias,
+}
 
 const map = L.map('map', {zoomControl: false});
 const setMap = () => {
@@ -27,6 +32,7 @@ let sidebar = L.control.sidebar({
 
 const showAllMarker = null;
 const markerGroup = L.layerGroup();
+
 function addMarker(filtros) {
     markerGroup.clearLayers();
     data.forEach(d => {
@@ -78,14 +84,17 @@ const filtroAsistencia = () => asistencias.reduce((acu, act) => {
     return acu += `<option value="${act.clave}">${act.info}</option>`;
 }, '')
 
-const agregarPanelFiltro = (sidebar, opcion, filtro) => {
+const agregarPanelFiltro = (sidebar, filtros) => {
 
-    const dataFiltrada = data.filter(d => d[filtro] === opcion.clave);
+    const dataFiltrada = data.filter(d => {
+        return (filtros.provincia === null || d.provincia === filtros.provincia) &&
+               (filtros.localidad === null || d.localidad === filtros.localidad) &&
+               (filtros.tipo === null || d.tipo === filtros.tipo);
+    });
     const items = dataFiltrada.reduce((acu, act) => {
         return acu += `
             <tr>
-                <td>${act.data.nombre}</td>
-                <td>${act.data.observacion}</td>
+                <td><p class="mb-0 font-weight-bold" style="color: #333; font-size: 1rem;">${act.data.nombre}</p></td>
                 <td>
                     <button type="button" class="btn btn-info irAlLugar" data-coordinate="${act.coordinate}">
                         Ir al Lugar
@@ -95,14 +104,32 @@ const agregarPanelFiltro = (sidebar, opcion, filtro) => {
         `
     }, "");
 
+    const capitalize = str => {
+        return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    }
+
+    const getInfoPorClave = (obj, clave) => {
+        const objPorClave = allFiltros[obj].find(prov => prov.clave === clave);
+        return objPorClave.info;
+    };
+
+    const filtrosSeleccionados = Object.entries(filtros).filter(([key, value]) => !!value)
+
+    const listFiltros = filtrosSeleccionados.reduce((acu, act) => {
+        return acu += `<li class="list-group-item">${capitalize(act[0])}: ${getInfoPorClave(act[0], act[1])}</li>`
+    }, "");
+
     const list = `
         <div class="container my-4 w-full">
+            <h4>Filtros seleccionados: <span class="badge badge-secondary">${filtrosSeleccionados.length}</span></h4>
+            <ul class="list-group my-2">
+                ${listFiltros}
+            </ul>
             <table class="table table-striped">
                 <thead class="thead-dark">
                 <tr>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Observación</th>
-                    <th scope="col">Ver</th>
+                    <th scope="col">Organización</th>
+                    <th scope="col" class"wx-2">Ver Lugar</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -116,7 +143,7 @@ const agregarPanelFiltro = (sidebar, opcion, filtro) => {
         id: 'panelFiltro',
         tab: '<i class="fa fa-check"></i>',
         pane: list,
-        title: `Ubicaciones según: ${opcion.info}`,
+        title: `Lugares Filtrados`,
         position: 'top',
     };
 
@@ -138,6 +165,7 @@ function filtros(sidebar) {
                 <select class="custom-select" id="selectProvincias">
                     <option selected class="d-none">Elije una provincia</option>
                     ${filtroProvincias()}
+                    <option value="not" >Sacar Filtro</option>
                 </select>
             </div>
             <div class="mb-3">
@@ -145,6 +173,7 @@ function filtros(sidebar) {
                 <select class="custom-select" id="selectLocalidades">
                     <option selected class="d-none">Elije una localidad</option>
                     ${filtroLocalidades()}
+                    <option value="not" >Sacar Filtro</option>
                 </select>
             </div>
             <div class="mb-3">
@@ -152,6 +181,7 @@ function filtros(sidebar) {
                 <select class="custom-select" id="selectAsistencia">
                     <option selected class="d-none">Elije un tipo</option>
                     ${filtroAsistencia()}
+                    <option value="not" >Sacar Filtro</option>
                 </select>
             </div>
             <div class="container mt-4">
@@ -182,22 +212,22 @@ function filtros(sidebar) {
     sidebar.addPanel(panelContent);
 
     document.querySelector("#selectProvincias").addEventListener("change", function (e) {
-        filtroElegida.provincia = e.target.value;
+        filtroElegida.provincia = e.target.value = e.target.value === 'not' ? null : e.target.value;;
         addMarker(filtroElegida);
     });
 
     document.querySelector("#selectLocalidades").addEventListener("change", function (e) {
-        filtroElegida.localidad = e.target.value;
+        filtroElegida.localidad = e.target.value = e.target.value === 'not' ? null : e.target.value;;
         addMarker(filtroElegida);
     });
 
     document.querySelector("#selectAsistencia").addEventListener("change", function (e) {
-        filtroElegida.tipo = e.target.value;
+        filtroElegida.tipo = e.target.value = e.target.value === 'not' ? null : e.target.value;;
         addMarker(filtroElegida);
     });
 
     document.querySelector("#verSegunFiltro").addEventListener("click", e => {
-        if (opcionElegida && filtroElegida) {
+        if (filtroElegida) {
             sidebar.removePanel("panelFiltro");
             agregarPanelFiltro(sidebar, filtroElegida);
             document.querySelectorAll('.irAlLugar').forEach(boton => {
